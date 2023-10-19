43f446eafe2a8d68107020ca3aacce9fb2a0521f
John Darrington <john.darrington@inl.gov>
2023-10-19 13:56:58 -0600

    only interpret a few modules, not the whole deps folder

--- lib/ingest_web/controllers/oidc_controller.ex
+++ lib/ingest_web/controllers/oidc_controller.ex
@@ -52,5 +52,49 @@ defmodule IngestWeb.OidcController do
   end
 
   def okta(conn, %{"code" => code}) do
+    config = Application.get_env(:ingest, :openid_connect_oneid)
+
+    with {:ok, token} <-
+           Oidcc.retrieve_token(
+             code,
+             Ingest.Application.Okta,
+             config[:client_id],
+             config[:client_secret],
+             %{redirect_uri: config[:redirect_uri]}
+           ) do
+      with {:ok, claims} <-
+             Oidcc.retrieve_userinfo(
+               token,
+               Ingest.Application.Okta,
+               config[:client_id],
+               config[:client_secret],
+               %{expected_subject: "sub"}
+             ) do
+        user = Accounts.get_user_by_email(claims["email"])
+
+        case user do
+          nil ->
+            with {:ok, user} <- Accounts.register_user(%{email: claims["email"]}, :oidcc) do
+              UserAuth.log_in_user(conn, user, %{})
+            else
+              {:error, err} ->
+                conn
+                |> put_flash(:error, "unable to get register user #{err}")
+                |> redirect(to: "/")
+            end
+
+          _ ->
+            UserAuth.log_in_user(conn, user, %{})
+        end
+      else
+        {:error, {err, status_code, %{"error" => error}}} ->
+          conn |> put_flash(:error, "unable to get user info #{error}") |> redirect(to: "/")
+      end
+    else
+      {:error, {err, status_code, %{"error" => error}}} ->
+        conn
+        |> put_flash(:error, "unable to get authorization token #{error}")
+        |> redirect(to: "/")
+    end
   end
 end
