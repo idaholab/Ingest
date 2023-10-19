5e3be74207c9f9eafd418893e52e9f5b047e004b
John Darrington <john.darrington@inl.gov>
2023-10-18 19:06:12 -0600

    pulling off into components

--- lib/ingest_web/live/dashboard_live/dashboard_live.ex
+++ lib/ingest_web/live/dashboard_live/dashboard_live.ex
@@ -2,6 +2,6 @@ defmodule IngestWeb.DashboardLive do
   use IngestWeb, :live_view
 
   def mount(params, session, socket) do
-    {:ok, socket, layout: {IngestWeb.Layouts, :dashboard}}
+    {:ok, socket |> assign(:section, "dashboard"), layout: {IngestWeb.Layouts, :dashboard}}
   end
 end
