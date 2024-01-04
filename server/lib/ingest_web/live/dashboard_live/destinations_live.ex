687d897445030b7ca26c6887977c5b2f7e047fd9
John W. Darrington <John.Darrington@inl.gov>
2024-01-03 18:34:29 -0700

    commit before the generation

--- server/lib/ingest_web/live/dashboard_live/destinations_live.ex
+++ server/lib/ingest_web/live/dashboard_live/destinations_live.ex
@@ -11,19 +11,91 @@ defmodule IngestWeb.DestinationsLive do
   def render(assigns) do
     ~H"""
     <div>
+      <div :if={!@destinations || Enum.empty?(@destinations)} class="text-center">
+        <.icon name="hero-folder-plus" class="mx-auto h-12 w-12 text-gray-400" />
+        <h3 class="mt-2 text-sm font-semibold text-gray-900">No destinations</h3>
+        <p class="mt-1 text-sm text-gray-500">Get started by creating a new destination.</p>
+        <.link
+          patch={~p"/dashboard/destinations/new"}
+          type="button"
+          class="mt-5 inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          <.icon name="hero-plus" /> New Destination
+        </.link>
+      </div>
+
+      <div :if={@destinations && !Enum.empty?(@destinations)} class="px-4 sm:px-6 lg:px-8">
+        <div class="sm:flex sm:items-center">
+          <div class="sm:flex-auto">
+            <h1 class="text-base font-semibold leading-6 text-gray-900">Data Destinations</h1>
+            <p class="mt-2 text-sm text-gray-700">
+              A list of all the destinations. A data destination is the location your data will be moved to after a user uploads them via a Data Request.
+            </p>
+          </div>
+          <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+            <div class="mt-6">
+              <.link patch={~p"/dashboard/destinations/new"}>
+                <button
+                  type="button"
+                  class="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  <.icon name="hero-plus" /> New Destination
+                </button>
+              </.link>
+            </div>
+          </div>
+        </div>
+        <div class="mt-8 flow-root">
+          <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+              <.table
+                id="destinations"
+                rows={@streams.destinations}
+                row_click={
+                  fn {_id, destination} -> JS.navigate(~p"/dashboard/destinations/#{destination}") end
+                }
+              >
+                <:col :let={{_id, destination}} label="Name"><%= destination.name %></:col>
+                <:col :let={{_id, destination}} label="Description">
+                  <%= destination.description %>
+                </:col>
+
+                <:action :let={{_id, destination}}>
+                  <.link
+                    navigate={~p"/dashboard/destinations/#{destination}"}
+                    class="text-indigo-600 hover:text-indigo-900"
+                  >
+                    Show
+                  </.link>
+                </:action>
+                <:action :let={{id, destination}}>
+                  <.link
+                    class="text-red-600 hover:text-red-900"
+                    phx-click={JS.push("delete", value: %{id: destination.id}) |> hide("##{id}")}
+                    data-confirm="Are you sure?"
+                  >
+                    Delete
+                  </.link>
+                </:action>
+              </.table>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <.modal
-        :if={@live_action == :register_client}
-        id="register_client_modal"
+        :if={@live_action in [:new]}
+        id="destination_modal"
         show
         on_cancel={JS.patch(~p"/dashboard/destinations")}
       >
         <.live_component
-          client={@client}
-          module={IngestWeb.LiveComponents.RegisterClientForm}
-          id="register-client-modal-component"
-          patch={~p"/dashboard/destinations/"}
-          current_user={@current_user}
           live_action={@live_action}
+          destination_form={@destination_form}
+          destination={@destination}
+          module={IngestWeb.LiveComponents.destinationForm()}
+          id="destination-modal-component"
+          current_user={@current_user}
         />
       </.modal>
     </div>
