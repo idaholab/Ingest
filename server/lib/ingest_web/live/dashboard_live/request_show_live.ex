916254b5d1eb4ec7a4420dc2d834057743b21478
John W. Darrington <John.Darrington@inl.gov>
2024-01-28 12:19:54 -0700

    added preview button

--- server/lib/ingest_web/live/dashboard_live/request_show_live.ex
+++ server/lib/ingest_web/live/dashboard_live/request_show_live.ex
@@ -308,11 +308,18 @@ defmodule IngestWeb.RequestShowLive do
                 <!-- TODO: Complete email section -->
                     Send invite
               </button>
+
               <button
                 type="submit"
                 class="ml-4 flex-shrink-0 rounded-md bg-gray-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Copy Link
+              </button>
+              <button
+                type="submit"
+                class="ml-4 flex-shrink-0 rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Preview
               </button>
             </form>
           </div>
