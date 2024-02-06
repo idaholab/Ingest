566705053007bff38b5062c2f5467ce853e9c7ce
John W. Darrington <John.Darrington@inl.gov>
2024-02-06 09:11:10 -0700

    remove erlazure

--- server/mix.exs
+++ server/mix.exs
@@ -25,7 +25,7 @@ defmodule Ingest.MixProject do
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_), do: ["lib", "erlazure"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Specifies your project dependencies.
   #
