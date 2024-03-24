6a65108aa26e539d5caab9a581008f3bd1cfad2f
John W. Darrington <John.Darrington@inl.gov>
2024-03-23 20:19:33 -0400

    indicator when metadata has been successfully completed

--- server/lib/ingest_web/live/dashboard_live/metadata_entry_live.ex
+++ server/lib/ingest_web/live/dashboard_live/metadata_entry_live.ex
@@ -86,13 +86,50 @@ defmodule IngestWeb.MetadataEntryLive do
         />
       <% end %>
 
-      <div class="flex bg-white shadow sm:rounded-lg justify-center">
+      <div
+        :if={
+          length(Enum.filter(@upload.metadatas, fn u -> u.submitted end)) != length(@upload.metadatas)
+        }
+        class="flex bg-white shadow sm:rounded-lg justify-center"
+      >
         <div class="px-4 py-5 sm:p-6">
           <h3 class="text-base font-semibold leading-6 text-gray-900">Complete Entry Task</h3>
           <div class="mt-2 max-w-xl text-sm text-gray-500">
             <p>
               Once you have completed and submitted all sections, this task will automatically complete.
             </p>
+          </div>
+        </div>
+      </div>
+
+      <div
+        :if={
+          length(Enum.filter(@upload.metadatas, fn u -> u.submitted end)) == length(@upload.metadatas)
+        }
+        class="rounded-md bg-green-50 p-4"
+      >
+        <div class="flex">
+          <div class="flex-shrink-0">
+            <svg
+              class="h-5 w-5 text-green-400"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </div>
+          <div class="ml-3">
+            <h3 class="text-sm font-medium text-green-800">Task completed</h3>
+            <div class="mt-2 text-sm text-green-700">
+              <p>
+                Metadata successfully submitted. You can navigate away from this page.
+              </p>
+            </div>
           </div>
         </div>
       </div>
@@ -124,7 +161,7 @@ defmodule IngestWeb.MetadataEntryLive do
   end
 
   @impl true
-  def handle_info({IngestWeb.LiveComponents.MetadataEntryForm, {:saved, metadata}}, socket) do
+  def handle_info({IngestWeb.LiveComponents.MetadataEntryForm, {:saved, _metadata}}, socket) do
     {:noreply,
      socket
      |> put_flash(:info, "Section saved successfully")
