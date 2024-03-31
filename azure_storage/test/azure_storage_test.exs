558ca2cbdb849a61553348ed2f59ef5184a763a1
John W. Darrington <John.Darrington@inl.gov>
2024-03-31 13:48:59 -0400

    got a ton of the underlying genserver completed

--- azure_storage/test/azure_storage_test.exs
+++ azure_storage/test/azure_storage_test.exs
@@ -3,6 +3,7 @@ defmodule AzureStorageTest do
   doctest AzureStorage
   alias AzureStorage.Container
   alias AzureStorage.Blob
+  import Config
 
   defp azurite_config do
     %AzureStorage.Config{
@@ -48,6 +49,49 @@ defmodule AzureStorageTest do
         |> Blob.put_block(azurite_config(), "second testing data")
 
       {:ok, _nil} = Blob.put_block_list([first, second], blob, azurite_config())
+    end
+  end
+
+  describe "the provided genserver" do
+    test "can upload a blob" do
+      {:ok, pid} =
+        AzureStorage.start_link(
+          account_name: "devstoreaccount1",
+          account_key:
+            "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==",
+          ssl: false,
+          base_service_url: "127.0.0.1:10000/devstoreaccount1"
+        )
+
+      {:ok, container} = AzureStorage.new_container(pid, "test")
+
+      {:ok, blob} =
+        pid |> AzureStorage.put_blob(container, "genservertest.txt", "genserver testing")
+
+      assert(blob.name == "genservertest.txt")
+    end
+
+    test "can upload a block and commit a blocklist" do
+      {:ok, pid} =
+        AzureStorage.start_link(
+          account_name: "devstoreaccount1",
+          account_key:
+            "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==",
+          ssl: false,
+          base_service_url: "127.0.0.1:10000/devstoreaccount1"
+        )
+
+      {:ok, container} = AzureStorage.new_container(pid, "test")
+      {:ok, blob} = AzureStorage.new_blob(pid, container, "blocklistcommittest.txt")
+
+      {:ok, first_block_id} =
+        pid |> AzureStorage.put_block(blob, "genserver block testing")
+
+      {:ok, second_block_id} =
+        pid |> AzureStorage.put_block(blob, "genserver block testing 2")
+
+      {:ok, _nil} =
+        pid |> AzureStorage.commit_blocklist(blob, [first_block_id, second_block_id])
     end
   end
 end
