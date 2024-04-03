00b988cb3058920b58ad989b0c2d1e51dae2b144
Travis Sage <travis.sage@inl.gov>
2024-04-03 13:23:29 -0600

    Fix DB settings

--- server/config/dev.exs
+++ server/config/dev.exs
@@ -3,10 +3,10 @@ import Config
 # Configure your database
 config :ingest, Ingest.Repo,
   username: "postgres",
-  password: "",
+  password: "monkwer88",
   hostname: "localhost",
   port: 5432,
-  database: "ingest",
+  database: "ingest_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
