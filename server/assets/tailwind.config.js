b800bf4a56fca538de13e8d05080e47554c819a7
John W. Darrington <John.Darrington@inl.gov>
2024-09-04 11:20:08 -0400

    ....

--- server/assets/tailwind.config.js
+++ server/assets/tailwind.config.js
@@ -1,14 +1,26 @@
 // See the Tailwind configuration guide for advanced usage
 // https://tailwindcss.com/docs/configuration
+
+const plugin = require("tailwindcss/plugin");
+const fs = require("fs");
+const path = require("path");
+
 module.exports = {
   daisyui: {
     themes: [
       {
         light: {
+          ...require("daisyui/src/theming/themes").light,
           primary: "#1d4ed8",
           "primary-content": "white",
           secondary: "#f39325",
           "secondary-content": "white",
+        },
+        dark: {
+          ...require("daisyui/src/theming/themes").dark,
+        },
+        cyberpunk: {
+          ...require("daisyui/src/theming/themes").cyberpunk,
         },
       },
     ],
