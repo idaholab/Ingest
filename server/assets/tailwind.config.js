8ed59c0e9489dc4304474f853ac4e9cb7a851625
John W. Darrington <John.Darrington@inl.gov>
2024-09-04 10:58:04 -0400

    quick fix

--- server/assets/tailwind.config.js
+++ server/assets/tailwind.config.js
@@ -1,10 +1,5 @@
 // See the Tailwind configuration guide for advanced usage
 // https://tailwindcss.com/docs/configuration
-
-const plugin = require("tailwindcss/plugin");
-const fs = require("fs");
-const path = require("path");
-
 module.exports = {
   daisyui: {
     themes: [
