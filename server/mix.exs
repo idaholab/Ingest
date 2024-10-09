9f14bfd5bfe4ed15d453a892dab7b3333ba6d526
John W. Darrington <John.Darrington@inl.gov>
2024-10-09 09:25:59 -0400

    quick fix to mix

--- server/mix.exs
+++ server/mix.exs
@@ -88,8 +88,12 @@ defmodule Ingest.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
-      "assets.build": ["tailwind default", "esbuild default", "cmd cd assets && npm install"],
+      "assets.setup": [
+        "tailwind.install --if-missing",
+        "esbuild.install --if-missing",
+        "cmd cd assets && npm install"
+      ],
+      "assets.build": ["tailwind default", "esbuild default"],
       "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
     ]
   end
