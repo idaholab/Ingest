433319ac4b1fb2a1d860a0a6315e24614463bcc8
John W. Darrington <John.Darrington@inl.gov>
2024-11-05 11:00:33 -0500

    release update

---
+++ server/apps/ingest_web/rel/overlays/bin/migrate.bat
@@ -0,0 +1,1 @@
+call "%~dp0\ingest_web" eval IngestWeb.Release.migrate
--- server/mix.exs
+++ server/mix.exs
@@ -7,7 +7,15 @@ defmodule Ingest.Umbrella.MixProject do
       version: "0.1.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      aliases: aliases()
+      aliases: aliases(),
+      releases: [
+        ingest_umbrella: [
+          applications: [
+            ingest: :permanent,
+            ingest_web: :permanent
+          ]
+        ]
+      ]
     ]
   end
 
