ed5612083b6c854c9064e825395b7f78579c2b78
John W. Darrington <John.Darrington@inl.gov>
2024-11-06 19:08:28 -0500

    fixed?

--- server/apps/ingest_web/lib/ingest_web/endpoint.ex
+++ server/apps/ingest_web/lib/ingest_web/endpoint.ex
@@ -1,5 +1,5 @@
 defmodule IngestWeb.Endpoint do
-  use Phoenix.Endpoint, otp_app: :ingest
+  use Phoenix.Endpoint, otp_app: :ingest_web
 
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
--- server/config/runtime.exs
+++ server/config/runtime.exs
@@ -17,7 +17,7 @@ import Config
 # Alternatively, you can use `mix phx.gen.release` to generate a `bin/server`
 # script that automatically sets the env var above.
 if System.get_env("PHX_SERVER") do
-  config :ingest, IngestWeb.Endpoint, server: true
+  config :ingest_web, IngestWeb.Endpoint, server: true
 end
 
 if config_env() == :prod do
@@ -68,7 +68,7 @@ if config_env() == :prod do
   host = System.get_env("PHX_HOST") || "example.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
-  config :ingest, IngestWeb.Endpoint,
+  config :ingest_web, IngestWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
     http: [
       # Enable IPv6 and bind on all interfaces.
