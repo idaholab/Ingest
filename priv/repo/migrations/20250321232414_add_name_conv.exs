f72337b3c512c53049663ba01bab79f27a3e5a27
Jen <jennifer.rogers1207@gmail.com>
2025-03-21 17:46:23 -0600

    migrated new parameter for requests

---
+++ priv/repo/migrations/20250321232414_add_name_conv.exs
@@ -0,0 +1,10 @@
+defmodule Ingest.Repo.Migrations.AddNameConv do
+  use Ecto.Migration
+
+  def change do
+    alter table(:requests) do
+      add :file_name_convention, {:array, :string}
+    end
+
+  end
+end
